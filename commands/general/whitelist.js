07621dc89bf18fee55a1b2d485ba0678054b4dfe
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-12-20 16:07:44 +0100

    fixed small issue with async handling

--- commands/general/whitelist.js
+++ commands/general/whitelist.js
@@ -3,20 +3,20 @@ const db = require('../../database/blacklist');
 module.exports = {
 	name: 'whitelist',
 	aliases: ['-wl'],
-	description: 'Whitelist tag-response commands for this user on a global scope. Same as `-j blacklist set global whitelist`',
+	description: 'Whitelist tag-response commands for this user on a global scope.',
 	usages: ['whitelist'],
 
 	async execute(message, args) {
 		const database = message.client.database;
 		const user = message.author;
-		const status = await db.getGlobalBlacklisted(database, user);
+		const whitelisted = (await db.getGlobalBlacklisted(database, user) == db.BlacklistStatus.whitelisted);
 
-		if (status == db.BlacklistStatus.whitelist) {
+		if (whitelisted) {
 			await message.reply('you already whitelist tag-response commands on a global scope');
 			return;
 		}
 
 		await db.setGlobalBlacklisted(database, user, false);
-		await message.reply('you now whitelists tag-response commands on a global scope');
+		await message.reply('you now whitelist tag-response commands on a global scope');
 	},
 };
