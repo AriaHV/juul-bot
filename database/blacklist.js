5b8b2cfef03910f29aabc89c36e36dcf9f6ccff0
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-12-20 13:30:18 +0100

    Tried to fix query issue.

--- database/blacklist.js
+++ database/blacklist.js
@@ -1,24 +1,24 @@
 const { Database, normaliseId } = require('./db');
-const { queries } = require('./db').queries();
+const { queries } = require('./db');
 
 const BlacklistStatus = Object.freeze({ blacklist:0, inherit: 1, whitelist: 2 });
 
 const getGlobalBlacklisted = async (database, user) => {
 	// TODO: remove logging when done
-	console.log('[blacklist.getGlobalBlacklister]:\t' + queries);
+	console.log('[blacklist.getGlobalBlacklisted]:\t' + queries());
 
-	const result = await database.query(queries['global-blacklist.get'], [normaliseId(user.id)]);
+	const result = await database.query(queries()['global-blacklist.get'], [normaliseId(user.id)]);
 	return (result.rows.length > 0) ? BlacklistStatus.blacklist : BlacklistStatus.whitelist;
 };
 
 const setGlobalBlacklisted = (database, user, blacklisted) => {
-	const query = (blacklisted ? queries['global-blacklist.add'] : queries['global-blacklist.delete']);
+	const query = (blacklisted ? queries()['global-blacklist.add'] : queries()['global-blacklist.delete']);
 	return database.query(query, [normaliseId(user.id)]);
 };
 
 const getGuildBlacklistStatus = async (database, user, guild) => {
 	const result = await database.query(
-		queries['guild-blacklist.get'],
+		queries()['guild-blacklist.get'],
 		[normaliseId(user.id), normaliseId(guild.id)]);
 
 	if (result.rows.length == 0) { return BlacklistStatus.inherit; }
@@ -27,7 +27,7 @@ const getGuildBlacklistStatus = async (database, user, guild) => {
 
 const setGuildBlacklistStatus = (database, user, guild, status) => {
 	if (status == BlacklistStatus.inherit) {
-		return database.query(queries['guild-blacklist.delete'], [normaliseId(user.id), normaliseId(guild.id)]);
+		return database.query(queries()['guild-blacklist.delete'], [normaliseId(user.id), normaliseId(guild.id)]);
 	}
 	else {
 		if (!(status == BlacklistStatus.blacklist || BlacklistStatus.whitelist)) {
@@ -36,13 +36,13 @@ const setGuildBlacklistStatus = (database, user, guild, status) => {
 		}
 
 		const args = [normaliseId(user.id), normaliseId(guild.id), (status == BlacklistStatus.blacklist)];
-		return database.query(queries['guild-blacklist.add'], args);
+		return database.query(queries()['guild-blacklist.add'], args);
 	}
 };
 
 const getChannelBlacklistStatus = async (database, user, channel) => {
 	const result = await database.query(
-		queries['channel-blacklist.get'],
+		queries()['channel-blacklist.get'],
 		[normaliseId(user.id), normaliseId(channel.id)]);
 
 	if (result.rows.length == 0) { return BlacklistStatus.inherit; }
@@ -51,7 +51,7 @@ const getChannelBlacklistStatus = async (database, user, channel) => {
 
 const setChannelBlacklistStatus = (database, user, channel, status) => {
 	if (status == BlacklistStatus.inherit) {
-		return database.query(queries['channel-blacklist.delete'],
+		return database.query(queries()['channel-blacklist.delete'],
 			[normaliseId(user.id), normaliseId(channel.id)]);
 	}
 	else {
@@ -60,7 +60,7 @@ const setChannelBlacklistStatus = (database, user, channel, status) => {
 		}
 
 		const args = [normaliseId(user.id), normaliseId(channel.id), (status == BlacklistStatus.blacklist)];
-		return database.query(queries['channel-blacklist.add'], args);
+		return database.query(queries()['channel-blacklist.add'], args);
 	}
 };
 
