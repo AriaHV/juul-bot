3c5f27f9ca112586d43016771e36e19436dca7d8
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-12-21 17:40:31 +0100

    Tried to fix issue in response command handling

--- database/response.js
+++ database/response.js
@@ -1,7 +1,7 @@
 const { Database, normaliseId } = require('./db');
 const { queries } = require('./db');
 
-const queryString = (database, name) => database.queries()[name];
+const queryString = (database, name) => database.queries[name];
 
 const getResponseEntries = async (database, name) => {
 	return database.query(queryString(database, 'response-command-entries.get'), [name]);
