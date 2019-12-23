166a2dbb700046bf702c660354636073e34f940c
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-12-23 21:34:30 +0100

    removed redundant require

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 require('dotenv').config();
-const fs = require('fs');
 const Discord = require('discord.js');
 const { Database } = require('./database/db.js');
-const { getCommands, isBotAuthor, sendResponseCommand } = require('./utils');
-const response = require('./commands/response/response');
 const { getPrefixes, getPrefixedMessage } = require('./utils/prefixes');
 const { getFamilies } = require('./utils/families');
 
 const client = new Discord.Client();
 client.database = new Database();
-client.commands = getCommands();
 client.families = getFamilies();
-console.log(client.families);
-console.log(client.families['general']);
 client.testing = process.env.DISCORD_TOKEN ? false : true;
 client.prefixes = getPrefixes(client.database, client.testing);
-console.log(client.prefixes);
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -35,7 +28,7 @@ client.on('message', async message => {
 
 	if(await client.families['general'].main.execute(message, args)) return;
 
-	const family = client.families[args[0].toLowerCase()];
+	const family = client.families[args[0].	toLowerCase()];
 	if (family && family.main) {
 		if (family.valid && !family.valid(message, args)) return;
 		await family.main.excute(message, args);
